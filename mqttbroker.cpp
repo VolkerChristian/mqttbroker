bbfb64395dc946dbc59b222a10b08f7960fd95c1
Volker Christian <volker.christian@fh-hagenberg.at>
2022-10-02 10:02:29 +0200

    More log output

--- mqttbroker.cpp
+++ mqttbroker.cpp
@@ -28,6 +28,14 @@
 #include "net/in/stream/tls/SocketServer.h"
 #include "net/un/stream/legacy/SocketServer.h"
 
+#include <openssl/asn1.h>
+#include <openssl/crypto.h>
+#include <openssl/obj_mac.h>
+#include <openssl/ssl3.h>
+#include <openssl/types.h>
+#include <openssl/x509.h>
+#include <openssl/x509v3.h>
+
 #endif // DOXYGEN_SHOUÖD_SKIP_THIS
 
 int main(int argc, char* argv[]) {
@@ -40,12 +48,27 @@ int main(int argc, char* argv[]) {
         "legacyin",
         []([[maybe_unused]] LegacyInSocketConnection* socketConnection) -> void { // OnConnect
             VLOG(0) << "OnConnect";
+
+            VLOG(0) << "\tLocal: (" + socketConnection->getLocalAddress().address() + ") " + socketConnection->getLocalAddress().toString();
+            VLOG(0) << "\tPeer:  (" + socketConnection->getRemoteAddress().address() + ") " +
+                           socketConnection->getRemoteAddress().toString();
+
         },
         []([[maybe_unused]] LegacyInSocketConnection* socketConnection) -> void { // OnConnected
             VLOG(0) << "OnConnected";
+
+            VLOG(0) << "\tLocal: (" + socketConnection->getLocalAddress().address() + ") " + socketConnection->getLocalAddress().toString();
+            VLOG(0) << "\tPeer:  (" + socketConnection->getRemoteAddress().address() + ") " +
+                           socketConnection->getRemoteAddress().toString();
+
         },
         []([[maybe_unused]] LegacyInSocketConnection* socketConnection) -> void { // OnDisconnected
             VLOG(0) << "OnDisconnected";
+
+            VLOG(0) << "\tLocal: (" + socketConnection->getLocalAddress().address() + ") " + socketConnection->getLocalAddress().toString();
+            VLOG(0) << "\tPeer:  (" + socketConnection->getRemoteAddress().address() + ") " +
+                           socketConnection->getRemoteAddress().toString();
+
         });
 
     mqttLegacyInServer.listen([mqttLegacyInServer](const MQTTLegacyInServer::SocketAddress& socketAddress, int errnum) mutable -> void {
@@ -70,12 +93,65 @@ int main(int argc, char* argv[]) {
         "tlsin",
         []([[maybe_unused]] TLSInSocketConnection* socketConnection) -> void { // OnConnect
             VLOG(0) << "OnConnect";
+            VLOG(0) << "\tLocal: (" + socketConnection->getLocalAddress().address() + ") " + socketConnection->getLocalAddress().toString();
+            VLOG(0) << "\tPeer:  (" + socketConnection->getRemoteAddress().address() + ") " +
+                           socketConnection->getRemoteAddress().toString();
+
         },
         []([[maybe_unused]] TLSInSocketConnection* socketConnection) -> void { // OnConnected
             VLOG(0) << "OnConnected";
+            X509* server_cert = SSL_get_peer_certificate(socketConnection->getSSL());
+            if (server_cert != nullptr) {
+                long verifyErr = SSL_get_verify_result(socketConnection->getSSL());
+
+                VLOG(0) << "\tPeer certificate: " + std::string(X509_verify_cert_error_string(verifyErr));
+
+                char* str = X509_NAME_oneline(X509_get_subject_name(server_cert), nullptr, 0);
+                VLOG(0) << "\t   Subject: " + std::string(str);
+                OPENSSL_free(str);
+
+                str = X509_NAME_oneline(X509_get_issuer_name(server_cert), nullptr, 0);
+                VLOG(0) << "\t   Issuer: " + std::string(str);
+                OPENSSL_free(str);
+
+                // We could do all sorts of certificate verification stuff here before deallocating the certificate.
+
+                GENERAL_NAMES* subjectAltNames =
+                    static_cast<GENERAL_NAMES*>(X509_get_ext_d2i(server_cert, NID_subject_alt_name, nullptr, nullptr));
+
+                int32_t altNameCount = sk_GENERAL_NAME_num(subjectAltNames);
+                VLOG(0) << "\t   Subject alternative name count: " << altNameCount;
+                for (int32_t i = 0; i < altNameCount; ++i) {
+                    GENERAL_NAME* generalName = sk_GENERAL_NAME_value(subjectAltNames, i);
+                    if (generalName->type == GEN_URI) {
+                        std::string subjectAltName =
+                            std::string(reinterpret_cast<const char*>(ASN1_STRING_get0_data(generalName->d.uniformResourceIdentifier)),
+                                        static_cast<std::size_t>(ASN1_STRING_length(generalName->d.uniformResourceIdentifier)));
+                        VLOG(0) << "\t      SAN (URI): '" + subjectAltName;
+                    } else if (generalName->type == GEN_DNS) {
+                        std::string subjectAltName =
+                            std::string(reinterpret_cast<const char*>(ASN1_STRING_get0_data(generalName->d.dNSName)),
+                                        static_cast<std::size_t>(ASN1_STRING_length(generalName->d.dNSName)));
+                        VLOG(0) << "\t      SAN (DNS): '" + subjectAltName;
+                    } else {
+                        VLOG(0) << "\t      SAN (Type): '" + std::to_string(generalName->type);
+                    }
+                }
+                sk_GENERAL_NAME_pop_free(subjectAltNames, GENERAL_NAME_free);
+
+                X509_free(server_cert);
+            } else {
+                VLOG(0) << "\tPeer certificate: no certificate";
+                // Here we can close the connection in case client didn't send a certificate
+            }
         },
         []([[maybe_unused]] TLSInSocketConnection* socketConnection) -> void { // OnDisconnected
             VLOG(0) << "OnDisconnected";
+
+            VLOG(0) << "\tLocal: (" + socketConnection->getLocalAddress().address() + ") " + socketConnection->getLocalAddress().toString();
+            VLOG(0) << "\tPeer:  (" + socketConnection->getRemoteAddress().address() + ") " +
+                           socketConnection->getRemoteAddress().toString();
+
         },
         options);
 
@@ -98,12 +174,27 @@ int main(int argc, char* argv[]) {
         "legacyun",
         []([[maybe_unused]] LegacyUnSocketConnection* socketConnection) -> void { // OnConnect
             VLOG(0) << "OnConnect";
+
+            VLOG(0) << "\tLocal: (" + socketConnection->getLocalAddress().address() + ") " + socketConnection->getLocalAddress().toString();
+            VLOG(0) << "\tPeer:  (" + socketConnection->getRemoteAddress().address() + ") " +
+                           socketConnection->getRemoteAddress().toString();
+
         },
         []([[maybe_unused]] LegacyUnSocketConnection* socketConnection) -> void { // OnConnected
             VLOG(0) << "OnConnected";
+
+            VLOG(0) << "\tLocal: (" + socketConnection->getLocalAddress().address() + ") " + socketConnection->getLocalAddress().toString();
+            VLOG(0) << "\tPeer:  (" + socketConnection->getRemoteAddress().address() + ") " +
+                           socketConnection->getRemoteAddress().toString();
+
         },
         []([[maybe_unused]] LegacyUnSocketConnection* socketConnection) -> void { // OnDisconnected
             VLOG(0) << "OnDisconnected";
+
+            VLOG(0) << "\tLocal: (" + socketConnection->getLocalAddress().address() + ") " + socketConnection->getLocalAddress().toString();
+            VLOG(0) << "\tPeer:  (" + socketConnection->getRemoteAddress().address() + ") " +
+                           socketConnection->getRemoteAddress().toString();
+
         });
 
     mqttLegacyUnServer.listen(
