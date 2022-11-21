5157e4fd1c261f4f734385f34decf84474225124
Volker Christian <volker.christian@fh-hagenberg.at>
2022-11-21 07:42:04 +0100

    Rename client instance in mqttintegrator

--- mqttintegrator/mqttintegrator.cpp
+++ mqttintegrator/mqttintegrator.cpp
@@ -83,7 +83,7 @@ int main(int argc, char* argv[]) {
                     }) doConnect{};
 
                 InMqttIntegratorClient inMqttIntegratorClient(
-                    "clientmapper",
+                    "mqttintegrator",
                     [](LegacyInSocketConnection* socketConnection) -> void {
                         VLOG(0) << "OnConnect";
 
